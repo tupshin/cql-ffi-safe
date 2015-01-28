657c40380f43c745879f079a4d9e6985e057c92a
Tupshin Harper <tupshin@tupshin.com>
2015-01-27 20:46:50 -0500

    cleaner map code

--- src/bin/map.rs
+++ src/bin/map.rs
@@ -8,21 +8,19 @@ use cql_ffi_safe::CassCollection;
 use cql_ffi_safe::CassCluster;
 use cql_ffi_safe::CassCollectionType;
 
-struct Pair<'a> {
-    key:&'a str,
-    value:i32
-}
 static CREATE_KEYSPACE_CMD:&'static str = "CREATE KEYSPACE IF NOT EXISTS examples WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '3' };";
 static CREATE_TABLE_CMD:&'static str = "CREATE TABLE IF NOT EXISTS examples.maps (key text, items map<text, int>, PRIMARY KEY (key))";
 static INSERT_QUERY_CMD:&'static str = "INSERT INTO examples.maps (key, items) VALUES (?, ?);";
 static SELECT_QUERY_CMD:&'static str = "SELECT items FROM examples.maps WHERE key = ?";
 
-fn insert_into_maps(session:&mut CassSession, key:&str, items:Vec<Pair>) -> Result<(),CassError> {
+static CONTACT_POINTS:&'static str = "127.0.0.1,127.0.0.2,127.0.0.3";
+
+fn insert_into_maps(session:&mut CassSession, key:&str, items:Vec<(&str,i32)>) -> Result<(),CassError> {
     let mut statement = CassStatement::new(INSERT_QUERY_CMD, 2);    
     let collection = CassCollection::new(CassCollectionType::MAP, items.len() as u64).unwrap();
     for item in items.iter() {
-        try!(collection.append_string(item.key));
-        try!(collection.append_int32(item.value));
+        try!(collection.append_string(item.0));
+        try!(collection.append_int32(item.1));
     }
     try!(statement.bind_string(0, key));
     try!(statement.bind_collection(1, collection));
@@ -50,23 +48,24 @@ fn select_from_maps(session: &mut CassSession, key:&str) -> Result<(),CassError>
 }    
 
 fn main() {
-    match CassCluster::new().set_contact_points("127.0.0.1,127.0.0.2,127.0.0.3") {
+    match CassCluster::new().set_contact_points(CONTACT_POINTS) {
+        Err(err) => panic!("{:?}",err),
         Ok(cluster) => {
             let mut session = CassSession::new();
             session.connect(cluster).wait();
-            let items:Vec<Pair> = vec!(
-                Pair{key:"apple", value:1 },
-                Pair{key:"orange", value:2 },
-                Pair{key:"banana", value:3 },
-                Pair{key:"mango", value:4 }
+            let items:Vec<(&str,i32)> = vec!(
+                ("apple", 1),
+                ("orange", 2),
+                ("banana", 3),
+                ("mango", 4)
             );
+            
             session.execute(CassStatement::new(CREATE_KEYSPACE_CMD,0));
             session.execute(CassStatement::new(CREATE_TABLE_CMD,0));
             insert_into_maps(&mut session, "test", items).unwrap();
             select_from_maps(&mut session, "test").unwrap();
             let close_future = session.close();
             close_future.wait();
-        },
-        Err(err) => panic!("{:?}",err)
+        }
     }
 }
