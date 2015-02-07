143113ac9e83250409d35f2646aaebb556a9d1ac
Tupshin Harper <tupshin@tupshin.com>
2015-02-07 09:35:58 -0500

    made simple.rs reflect upstream intent

--- src/bin/simple.rs
+++ src/bin/simple.rs
@@ -6,7 +6,6 @@ use cql_ffi_safe::CassSession;
 use cql_ffi_safe::CassCluster;
 use cql_ffi_safe::FromCol;
 use cql_ffi_safe::CassError;
-use cql_ffi_safe::CassString;
 
 static SELECT_QUERY_CMD:&'static str = "SELECT keyspace_name,durable_writes FROM system.schema_keyspaces;";
 static CONTACT_POINTS:&'static str = "127.0.0.1,127.0.0.2,127.0.0.3";
@@ -20,8 +19,7 @@ fn main() {
             let mut future = session.execute(&CassStatement::new(SELECT_QUERY_CMD,0));
             future.wait();
             for row in future.get_result().unwrap().iter() {
-                let value:Result<bool,CassError> = FromCol::from_col(row.get_column(1));
-                let value:Result<CassString,CassError> = row.get_column(0).get_string();
+                let value:Result<String,CassError> = FromCol::from_col(row.get_column(0));
                 match value {
                     Ok(ks) => {
                         println!("ks: {:?}",ks);
